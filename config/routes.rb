b50e5ec7f0bc97a7b3dc04d181b765fb0f52c6ea
Deonte Cooper <deonte.cooper12@gmail.com>
2019-04-30 17:38:10 -0600

    Made some changes to main app files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      namespace :merchants do
+        get '/find', to: 'search#show', as: :find_merchant
+        get '/find_all', to: 'search#index', as: :find_all_merchant
+        get '/:id/items', to: 'items#index', as: :items
+        get '/:id/invoices', to: 'invoices#index', as: :invoices
+        get '/random', to: 'random#show', as: :random_merchant
+      end
+      resources :merchants, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
 end

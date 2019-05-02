a946e13307b23349544fe8afe44839910defd809
Deonte Cooper <deonte.cooper12@gmail.com>
2019-05-02 12:18:36 -0600

    Made some changes to factory

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :transaction do
-    sequence(:credit_card_number) { |n| "489#{n}098#{n}502938#{n}" }
+    credit_card_number { "9358827631846591" }
     credit_card_expiration_date { "04/22" }
     result { "success" }
   end

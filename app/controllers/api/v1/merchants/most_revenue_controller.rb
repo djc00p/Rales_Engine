925ff3ac5e9cc3cad92a77179fd792496a000355
Deonte Cooper <deonte.cooper12@gmail.com>
2019-05-02 12:14:32 -0600

    Added controller for most_revenue

--- app/controllers/api/v1/merchants/most_revenue_controller.rb
+++ app/controllers/api/v1/merchants/most_revenue_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Merchants::MostRevenueController < Api::V1::Merchants::ApplicationController
   def show
-    render json: MerchantSerializer.new(Merchant.most_revenue())
+    render json: MerchantSerializer.new(Merchant.most_revenue(params[:quantity]))
   end
 end

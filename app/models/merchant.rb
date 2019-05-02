4c685c19a1dca2c4e7153b2e4a3e72937a958dd5
Deonte Cooper <deonte.cooper12@gmail.com>
2019-05-02 12:16:57 -0600

    Added Method most_items

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -11,4 +11,12 @@ class Merchant < ApplicationRecord
     .order('revenue DESC')
     .limit(count)
   end
+
+  def self.most_items(count)
+    select('merchants.*, SUM(invoice_items.quantity) as items_sold')
+    .joins(items: :invoice_items)
+    .group(:id)
+    .order('items_sold DESC')
+    .limit(count)
+  end
 end
